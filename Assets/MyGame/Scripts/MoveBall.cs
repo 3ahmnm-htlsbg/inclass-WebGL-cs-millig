9e4ba0c032fb1fd699f86fc2182eee5582cad9a2
Melissa Illig <melissaillig@Melissas-MacBook-Pro.local>
2020-01-14 15:13:44 +0100

    add Script to the Scene

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{   
+    //public GameObject ballPosTopLeft;
+    //public GameObject ballPosTopRight;
+    //public GameObject ballPosBottomLeft;
+    //public GameObject ballPosBottomRight;
+
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject ball;
+
+    public void MoveTheBall() 
+    {
+
+        Debug.Log(Random.Range(0,1));
+    }
+ 
+   
+
+
+ 
+}
